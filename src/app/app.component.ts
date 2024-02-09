4c759030ba39e58dd320a6510f5d132afe056fba
genildocs <genildocs@gmail.com>
2024-02-09 16:18:44 -0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { InputSearchComponent } from './components/input-search/input-search.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    HeaderComponent,
+    InputSearchComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
