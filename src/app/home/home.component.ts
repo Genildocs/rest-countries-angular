7b15e212867a4694cc2ef0aba8da46923fe5f0ae
genildocs <genildocs@gmail.com>
2024-02-13 13:35:04 -0300

    commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { ApiComponent } from '../api/api.component';
-
+import { CountriesComponent } from '../components/countries/countries.component';
+import { InputSearchComponent } from '../components/input-search/input-search.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ApiComponent],
+  imports: [InputSearchComponent, CountriesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "450px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    extend: {
+      colors: {
+        DarkBlue: "#2b3945",
+        VeryDarkBlue: "#202c37",
+        LightModeText: "#111517",
+        DarkGray: "#858585",
+        VeryLightGray: "#fafafa",
+        whiteText: "#ffffff",
+        Bluehover: "#066bc0",
+      },
+      gridTemplateColumns: {
+        "auto-fit": "repeat(auto-fit, minmax(5rem, 16.5rem))",
+      },
+
+      boxShadow: {
+        concreto: " 1px 1px 7px 0px rgba(0, 0, 0, 0.75)",
+      },
+    },
   },
   plugins: [],
 };
