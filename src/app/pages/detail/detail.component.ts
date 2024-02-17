fde9e5ce86a89512f335a17f9ba5000151bb032a
genildocs <genildocs@gmail.com>
2024-02-17 17:12:42 -0300

    commit

--- src/app/pages/detail/detail.component.ts
+++ src/app/pages/detail/detail.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import {ActivatedRoute} from "@angular/router";
+import { ActivatedRoute } from '@angular/router';
 import { CountrieDetail } from '../../interface/countries-detail';
-import {ApiService} from "../../service/api.service";
+import { ApiService } from '../../service/api.service';
 
 @Component({
     selector: 'app-detail',
@@ -12,27 +12,28 @@ import {ApiService} from "../../service/api.service";
     styleUrl: './detail.component.css',
 })
 export class DetailComponent {
-    countrie: any = []
+    countrie: any = [];
     countrieName: string = '';
-    constructor(private apiService: ApiService, private route: ActivatedRoute) {}
+    constructor(
+        private apiService: ApiService,
+        private route: ActivatedRoute
+    ) {}
 
-    ngOnInit(){
-        this.route.params.subscribe(params=>{
+    ngOnInit() {
+        this.route.params.subscribe((params) => {
             this.countrieName = params['countrieName'];
-        })
-        this.getCountrie(this.countrieName)
+        });
+        this.getCountrie(this.countrieName);
     }
-    getCountrie(param:string){
+    getCountrie(param: string) {
         this.apiService.getCountrie(param).subscribe({
-            next: (countre:any) => {
-            this.countrie = countre
+            next: (countre: any) => {
+                this.countrie = countre;
                 console.log('Busca concluída com sucesso!');
-        },
-            error: (err) =>
-            {console.log('Error' + err)}
-
-        })
+            },
+            error: (err) => {
+                console.log('Error' + err);
+            },
+        });
     }
-
-
 }
